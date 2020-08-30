92cb4f69833e659f705b71d208bed522741df239
caiyuan <caiyuan@live.com>
2020-08-30 13:00:14 +0800

    did-deep

--- did-deep.sh
+++ did-deep.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 
-    docker run --name docker-o -h docker-o -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
+    docker run --name docker-dind -h docker-dind -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
 
-sleep 15s; docker exec -it docker-o \
-    docker run --name docker-i -h docker-i -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
+sleep 10s; docker exec -it docker-dind \
+    docker run --name docker-1 -h docker-1 -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
 
-sleep 15s; docker exec -it docker-o docker exec -it docker-i \
+sleep 10s; docker exec -it docker-dind \
+    docker run --name docker-2 -h docker-2 -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
+
+sleep 10s; docker exec -it docker-dind docker exec -it docker-2 \
     docker run --name nginx -h nginx -d nginx
 
-sleep 5s; docker exec -it docker-o docker exec -it docker-i \
+sleep 5s; docker exec -it docker-dind docker exec -it docker-2 \
     wget -S -P /root 172.17.0.2:80
 
-    docker rm -f docker-o
+    docker rm -f docker-dind
