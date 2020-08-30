d185d46049ada9c8dfc14502732a64ccf8cfd774
caiyuan <caiyuan@live.com>
2020-08-30 14:06:38 +0800

    did-deep

--- did-deep.sh
+++ did-deep.sh
@@ -9,9 +9,12 @@ sleep 10s; docker exec -it docker-dind \
     docker run --name docker-2 -h docker-2 -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
 
 sleep 10s; docker exec -it docker-dind docker exec -it docker-2 \
-    docker run --name nginx -h nginx -d nginx
+    docker network create --driver bridge nginx-net
 
 sleep 5s; docker exec -it docker-dind docker exec -it docker-2 \
-    wget -S -P /root 172.17.0.2:80
+    docker run --name nginx -h nginx --network nginx-net -d nginx
+
+sleep 5s; docker exec -it docker-dind docker exec -it docker-2 \
+    wget -S -P /root 172.19.0.2:80
 
     docker rm -f docker-dind
