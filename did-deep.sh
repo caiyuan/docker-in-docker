3676f092d89de9b3406517720cc6c1365c95df40
caiyuan <caiyuan@live.com>
2020-08-30 16:26:57 +0800

    did-deep

--- did-deep.sh
+++ did-deep.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env sh
 
+##
     docker run --name docker-dind -h docker-dind -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
 
+##
 sleep 15s; docker exec -it docker-dind \
     docker run --name docker-1 -h docker-1 -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
 
 sleep 15s; docker exec -it docker-dind \
     docker run --name docker-2 -h docker-2 -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
 
+##
 sleep 15s; docker exec -it docker-dind docker exec -it docker-2 \
     docker network create --driver bridge nginx-net
 
-sleep 5s; docker exec -it docker-dind docker exec -it docker-2 \
+sleep  5s; docker exec -it docker-dind docker exec -it docker-2 \
     docker run --name nginx -h nginx --network nginx-net -d nginx
 
-sleep 15s; docker exec -it docker-dind docker exec -it docker-2 \
+##
+sleep 15s; docker exec -it docker-dind docker exec -it docker-1 \
+    ip route add 172.19.0.0/16 via 172.18.0.3 dev eth0
+
+sleep  5s; docker exec -it docker-dind docker exec -it docker-1 \
     wget -S -P /root 172.19.0.2:80
 
+##
     docker rm -f docker-dind
