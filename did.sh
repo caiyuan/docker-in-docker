2e2e4529750ef0d23fec07b9f5694c104d59e7ed
caiyuan <caiyuan@live.com>
2020-09-01 23:23:59 +0800

    did

--- did.sh
+++ did.sh
@@ -1,26 +1,42 @@
 #!/usr/bin/env sh
 
-##
+echo
+echo '启动守护进程使用自定义桥接网络'
+echo
 
-docker network create some-network
+docker network create dind-network
 
-docker run -d --privileged --name some-docker \
-	--network some-network --network-alias docker \
+docker run -d --privileged \
+	--name docker-dind \
+	--network dind-network \
+	--network-alias docker \
 	-e DOCKER_TLS_CERTDIR=/certs \
-	-v some-docker-certs-ca:/certs/ca \
-	-v some-docker-certs-client:/certs/client \
-		caiyuan/docker:19.03.12-dind
+	-v dind-certs-ca:/certs/ca \
+	-v dind-certs-client:/certs/client \
+	caiyuan/docker:19.03.12-dind
 
-echo 'Wait for the daemon to be ready.'; sleep 15s;
+echo
+echo '等待守护进程就绪 (15s)'
 
-docker run -it --rm --network some-network \
+sleep 15s
+
+echo
+echo ' 输出客户端和服务端的版本信息: '
+echo
+
+docker run -it --rm \
+	--network dind-network \
 	-e DOCKER_TLS_CERTDIR=/certs \
-	-v some-docker-certs-client:/certs/client:ro \
-		caiyuan/docker:19.03.12-zsh version
+	-v dind-certs-client:/certs/client:ro \
+	caiyuan/docker:19.03.12-zsh version
 
-##
+echo
+echo '执行痕迹清理'
+echo
 
-docker rm -f some-docker
-docker network rm some-network
-docker volume rm some-docker-certs-ca
-docker volume rm some-docker-certs-client
+docker rm -f docker-dind
+docker network rm dind-network
+docker volume rm dind-certs-ca
+docker volume rm dind-certs-client
+
+echo
