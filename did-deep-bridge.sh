442c353eecb319bbfbd80f522d14388e282ef1b1
caiyuan <caiyuan@live.com>
2020-09-01 15:11:08 +0800

    did-deep bridge

--- did-deep-bridge.sh
+++ did-deep-bridge.sh
@@ -1,70 +1,73 @@
 #!/usr/bin/env sh
 
 ##
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind \
+    docker run --name docker-3 -h docker-3 -d --privileged caiyuan/docker:19.03.12-dind --registry-mirror https://hub-mirror.c.163.com
+
+##
+sleep 15s
+
+##
+docker exec -it docker-dind docker exec -it docker-3 \
     docker run --name busybox-1 --network none -d caiyuan/busybox:1.32.0 sleep infinite
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker run --name busybox-2 --network none -d caiyuan/busybox:1.32.0 sleep infinite
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker run --name busybox-3 --network none -d caiyuan/busybox:1.32.0 sleep infinite
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker run --name busybox-4 --network none -d caiyuan/busybox:1.32.0 sleep infinite
 
 ##
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network create net-1
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network create net-3-1
 
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network create net-2
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network create net-3-2
 
 ##
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker network disconnect none busybox-1
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker network disconnect none busybox-2
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker network disconnect none busybox-3
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker network disconnect none busybox-4
 
 ##
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network connect net-1 busybox-1
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network connect net-3-1 busybox-1
 
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network connect net-1 busybox-2
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network connect net-3-1 busybox-2
 
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network connect net-1 busybox-3
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network connect net-3-1 busybox-3
 
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network connect net-2 busybox-3
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network connect net-3-2 busybox-3
 
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker network connect net-2 busybox-4
+docker exec -it docker-dind docker exec -it docker-3 \
+    docker network connect net-3-2 busybox-4
 
-docker exec -it docker-dind docker exec -it docker-1 \
+docker exec -it docker-dind docker exec -it docker-3 \
     docker network connect bridge busybox-4
 
 ##
-echo 'docker-1: brctl show'
-docker exec -it docker-dind docker exec -it docker-1 \
+echo 'docker-3: brctl show'
+docker exec -it docker-dind docker exec -it docker-3 \
     brctl show
 
-echo 'docker-1: ip link | grep UP'
-docker exec -it docker-dind docker exec -it docker-1 \
+echo 'docker-3: ip link | grep UP'
+docker exec -it docker-dind docker exec -it docker-3 \
     ip link | grep UP
 
 ##
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker rm -f busybox-1 busybox-2 busybox-3 busybox-4
-
-docker exec -it docker-dind docker exec -it docker-1 \
-    docker system prune -a
-
+docker exec -it docker-dind \
+    docker rm -f docker-3
