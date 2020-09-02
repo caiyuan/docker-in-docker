996039562d73139eae38ddfaf5cdc4ea9e29872c
caiyuan <caiyuan@live.com>
2020-09-02 12:14:36 +0800

    did-route

--- network/did-route.sh
+++ network/did-route.sh
@@ -10,7 +10,7 @@ docker exec -it docker-dind \
 docker exec -it docker-dind \
     docker run --name docker-2 -h docker-2 -d --privileged caiyuan/docker:19.03.12-dind
 
-sleep 15s; 
+sleep 15s
 
 echo
 echo '在 docker-1 上启动 busybox 容器并使用自定义网桥'
@@ -19,12 +19,27 @@ echo
 docker exec -it docker-dind docker exec -it docker-1 \
     docker network create \
     --driver bridge \
-    --subnet=172.28.0.0/16 \
-    --ip-range=172.28.0.0/24 \
-    --gateway=172.28.0.1 \
+    --subnet=11.0.0.0/16 \
+    --ip-range=11.0.0.0/24 \
+    --gateway=11.0.0.1 \
     local-net
 
 docker exec -it docker-dind docker exec -it docker-1 \
+    docker run --name busybox --network local-net -d caiyuan/busybox:1.32.0 sleep infinite
+
+echo
+echo '在 docker-2 上启动 busybox 容器并使用自定义网桥'
+echo
+
+docker exec -it docker-dind docker exec -it docker-2 \
+    docker network create \
+    --driver bridge \
+    --subnet=12.0.0.0/16 \
+    --ip-range=12.0.0.0/24 \
+    --gateway=12.0.0.1 \
+    local-net
+
+docker exec -it docker-dind docker exec -it docker-2 \
     docker run --name busybox --network local-net -d caiyuan/busybox:1.32.0 sleep infinite
 
 echo
@@ -49,10 +64,16 @@ docker exec -it docker-dind docker exec -it docker-2 \
     ip route
 
 echo
+echo '  busybox in docker-2: '
+
+docker exec -it docker-dind docker exec -it docker-2 \
+    docker exec -it busybox ip route
+
+echo
 echo '为 docker-2 添加路由并访问 busybox 容器'
 
 docker exec -it docker-dind docker exec -it docker-2 \
-    ip route add 172.28.0.0/16 via 172.18.0.2 dev eth0
+    ip route add 11.0.0.0/16 via 172.18.0.2 dev eth0
 
 echo
 echo '  docker-2: (2)'
@@ -61,5 +82,7 @@ docker exec -it docker-dind docker exec -it docker-2 \
     ip route
 
 echo
+echo '从 docker-2 busybox 访问 docker-1 busybox'
+
 docker exec -it docker-dind docker exec -it docker-2 \
-    ping 172.28.0.2
+    docker exec -it busybox ping 11.0.0.2
